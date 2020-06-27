b7a27db117ce42bf224484a4589ac7dfa7b7a776
Vladislav <vladislav14141@yandex.ru>
2020-06-27 15:17:24 +1000

    Initial Commit

---
+++ DesignCode SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode SwiftUI
+//
+//  Created by Миронов Влад on 27.06.2020.
+//  Copyright © 2020 Миронов Влад. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
